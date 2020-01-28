43665ae9f2449408cc2660ecf04bbdc116b75615
Sergio Carracedo <info@sergiocarracedo.es>
2020-01-28 09:55:50 +0100

    Publish build

--- sw.js
+++ sw.js
@@ -2,47 +2,43 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/3d308d091cd4975bcd98.css",
-    "revision": "2698c8cf920b29b277a3bec26658d75f"
+    "url": "/_nuxt/22236edace783dfe2cbf.js",
+    "revision": "53f9d0381a622ddb62b13c37f4a2efba"
   },
   {
-    "url": "/_nuxt/45e680ceadc85bc26f3d.js",
-    "revision": "6a72c9ef85a271845ce0c4eb9423df8a"
+    "url": "/_nuxt/2cb3ddef1a47633d1530.js",
+    "revision": "f4179a7e9c2ceef43e30c0b0b7736a52"
   },
   {
-    "url": "/_nuxt/70b2cb70c3cb708a7a48.js",
-    "revision": "39f0c9c57edd782b2048fcba86903329"
+    "url": "/_nuxt/430418e306825a0b8991.js",
+    "revision": "eb52bcc554226ad13c636297b1e7344d"
   },
   {
-    "url": "/_nuxt/7c22f49f2a7d53318c98.css",
-    "revision": "c7a6fcdf5c0d57f85c653de1c0a7271f"
+    "url": "/_nuxt/5538a7f12c5ed425b929.js",
+    "revision": "7092f3561d3de7d086dc04d221ce1489"
   },
   {
-    "url": "/_nuxt/8a555ea998dc7a73a5fe.js",
-    "revision": "6130df844fdbd8205437f7f1b02dde04"
+    "url": "/_nuxt/69eddb94cb3fc0510a66.css",
+    "revision": "941df2c0cfda3b11de2909451d4e6ea1"
   },
   {
-    "url": "/_nuxt/95a941e3207621294f4e.js",
-    "revision": "2a6fdccb49e8c87d8706153fe722ba78"
+    "url": "/_nuxt/9838b4f0d563e12d872f.css",
+    "revision": "585dac7f6af0a9d3611365be2343a2ef"
   },
   {
-    "url": "/_nuxt/a7454b4ad2467b1424d7.js",
-    "revision": "b6ac307625044e3206d88390941958fb"
+    "url": "/_nuxt/b5247770333959d04c91.js",
+    "revision": "df03d3135f2854a14dc1c482c40ec19e"
   },
   {
-    "url": "/_nuxt/aab3999ced0fac121b5f.js",
-    "revision": "8a72eee938710ea510fc2a8e8aa8f772"
+    "url": "/_nuxt/c926f7489537406caf75.js",
+    "revision": "8dc89f0806b850ae4e192ba7fd372028"
   },
   {
-    "url": "/_nuxt/c1b283bc0ee91d2101bb.js",
-    "revision": "0131efe09539ccf96563caeee1e316f9"
-  },
-  {
-    "url": "/_nuxt/ef40e4045b474f2e8ff4.js",
-    "revision": "191d15185fa9a57a098c94673578e60c"
+    "url": "/_nuxt/e00da40677dda1984c55.js",
+    "revision": "35a1bb4f98a8c2fd3b818f2bccbaadaf"
   }
 ], {
-  "cacheId": "PHPulpo-Con-2019",
+  "cacheId": "PHPulpo-Con-2020",
   "directoryIndex": "/",
   "cleanUrls": false
 })
